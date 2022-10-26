93ef83604dcf7241222cb85236e5fb8951c4dbab
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-10-26 20:13:21 +0900

    [level 2] Title: 프린터, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.cpp
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -1,30 +1,35 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    vector<int> vec = priorities;
+    queue<int> q;
+    vector<int> sorted;
     
-    while(true){
-        int max = *max_element(vec.begin(), vec.end());
-        if(vec[0] != max){
-            vec.push_back(vec[0]);
-            vec.erase(vec.begin());
-            location = location == 0 ? vec.size() - 1 : location - 1;
-        }
+    for(int i = 0; i < priorities.size(); i++)
+        q.push(i);
+    
+    while(!q.empty()){
+        int idx = q.front();
+        q.pop();
+        
+        if(priorities[idx] != *max_element(priorities.begin(), priorities.end()))
+            q.push(idx);
         else{
-            vec.erase(vec.begin());
-            answer++;
-            if(location == 0)
-                break;
-            location--;
+            sorted.push_back(idx);
+            priorities[idx] = 0;
         }
-        cout << location << ' ';
     }
+    
+    for(int i = 0; i < sorted.size(); i++)
+        if(sorted[i] == location){
+            answer = i + 1;
+            break;
+        }
     
     return answer;
 }
