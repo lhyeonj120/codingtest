b6da7e7ffa4f8b7800064d969cf9f5e7ca739132
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-10-27 17:37:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -5,28 +5,49 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0, remaining_weight = weight, idx = 0;
-    queue<int> bridge;
+//     int answer = 0, remaining_weight = weight, idx = 0;
+//     queue<int> bridge;
     
-    for(int i = 0; i < bridge_length; i++)
-        bridge.push(0);
+//     for(int i = 0; i < bridge_length; i++)
+//         bridge.push(0);
+    
+//     while(true){
+//         if(idx >= truck_weights.size())
+//             break;
+//         answer++;
+//         int n = bridge.front();
+//         bridge.pop();
+//         remaining_weight += n;
+//         if(remaining_weight >= truck_weights[idx]){
+//             bridge.push(truck_weights[idx]);
+//             remaining_weight -= truck_weights[idx++];
+//         }
+//         else{
+//             bridge.push(0);
+//         }
+//     }
+//     answer += bridge_length;
+    int answer = 0, idx = 0, time = 0, total_weight = 0;
+    // 트럭 무게, 다리를 건넌 시간
+    queue<pair<int, int>> truck_move;
     
     while(true){
-        if(idx >= truck_weights.size())
+        if(weight >= total_weight + truck_weights[idx]){
+            truck_move.push({truck_weights[idx], bridge_length + time + 1});
+            total_weight += truck_weights[idx++];
+        }
+        if(idx >= truck_weights.size()){
+            answer = truck_move.back().second;
             break;
-        answer++;
-        int n = bridge.front();
-        bridge.pop();
-        remaining_weight += n;
-        if(remaining_weight >= truck_weights[idx]){
-            bridge.push(truck_weights[idx]);
-            remaining_weight -= truck_weights[idx++];
         }
         else{
-            bridge.push(0);
+            time++;
+            if(truck_move.front().second == time + 1){
+                total_weight -= truck_move.front().first;
+                truck_move.pop();
+            }
         }
     }
-    answer += bridge_length;
     
     return answer;
 }
