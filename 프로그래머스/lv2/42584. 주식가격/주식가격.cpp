28e8160dc7a2c702abd3263b4db36ea5aad5bbed
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-10-27 18:45:16 +0900

    [level 2] Title: 주식가격, Time: 15.73 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -7,22 +7,37 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     vector<int> answer(prices.size());
     // 주식 가격, 인덱스 담을 스택
-    stack<pair<int, int>> s;
+//     stack<pair<int, int>> s;
     
-    for(int i = 0; i < prices.size(); i++){
-        if(s.empty()){
-            s.push({prices[i], i});
-            continue;
-        }
-        while(!s.empty() && s.top().first > prices[i]){
-            answer[s.top().second] = i - s.top().second;
+//     for(int i = 0; i < prices.size(); i++){
+//         if(s.empty()){
+//             s.push({prices[i], i});
+//             continue;
+//         }
+//         while(!s.empty() && s.top().first > prices[i]){
+//             answer[s.top().second] = i - s.top().second;
+//             s.pop();
+//         }
+//         s.push({prices[i], i});
+//     }
+    
+//     while(!s.empty()){
+//         answer[s.top().second] = prices.size() - 1 - s.top().second;
+//         s.pop();
+//     }
+    stack<int> s;
+    int size = prices.size();
+    
+    for(int i = 0; i < size; i++){
+        while(!s.empty() && prices[s.top()] > prices[i]){
+            answer[s.top()] = i - s.top();
             s.pop();
         }
-        s.push({prices[i], i});
+        s.push(i);
     }
     
     while(!s.empty()){
-        answer[s.top().second] = prices.size() - 1 - s.top().second;
+        answer[s.top()] = size - 1 - s.top();
         s.pop();
     }
     
