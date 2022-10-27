52eefb09af8e703f70f7cfc49f8c0dbb415d4d8a
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-10-28 00:57:43 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
@@ -1,55 +1,52 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
-bool isChange(string s1, string s2){
-    int n = 0, length = s1.length();
-    for(int i = 0; i < length; i++){
-        if(s1[i] != s2[i])
+bool visited[50];
+
+bool isPossible(string a, string b){
+    int n = 0;
+    
+    for(int i = 0; i < a.length(); i++)
+        if(a[i] != b[i])
             n++;
-        if(n >= 2)
-            return false;
-    }
-    return true;
+    if(n == 1)
+        return true;
+    return false;
 }
 
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    // 문자열 담을 큐
-    queue<string> q;
-    // 문자열, 몇 번째 단계 담을 map
-    unordered_map<string, int> cnt;
+    // 문자열, 횟수 담을 큐
+    queue<pair<string, int>> q;
     
     auto iter = find(words.begin(), words.end(), target);
     if(iter == words.end())
         return 0;
     
-    q.push(begin);
-    cnt[begin] = 0;
-    cnt[target] = -1;
-    for(auto word : words)
-        cnt[word] = -1;
+    q.push({begin, 0});
     
     while(!q.empty()){
         auto cur = q.front();
         q.pop();
         
-        for(int i = 0; i < words.size(); i++){
-            if(cnt[words[i]] >= 0)
-                continue;
-            if(!isChange(cur, words[i]))
-                continue;
-            q.push(words[i]);
-            cnt[words[i]] = cnt[cur] + 1;
-        }
-        if(cnt[target] != -1)
+        if(cur.first == target){
+            answer = cur.second;
             break;
+        }
+        
+        for(int i = 0; i < words.size(); i++){
+            if(visited[i])
+                continue;
+            if(!isPossible(cur.first, words[i]))
+                continue;
+            q.push({words[i], cur.second + 1});
+            visited[i] = true;
+        }
     }
-    answer = cnt[target];
     
     return answer;
 }
