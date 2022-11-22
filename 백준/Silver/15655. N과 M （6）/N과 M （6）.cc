6562bd814898948a9d181e8915f4c3e6f795565e
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-11-22 18:31:32 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -1,30 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
+int N, M;
 int num[10];
+bool used[10];
+
+void permutation(int idx, int cnt){
+    if(cnt == M){
+        for(int i = 0; i < N; i++)
+            if(used[i])
+                cout << num[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = idx; i < N; i++){
+        if(!used[i]){
+            used[i] = true;
+            permutation(i + 1, cnt + 1);
+            used[i] = false;
+        }
+    }
+}
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int N, M;
     cin >> N >> M;
     for(int i = 0; i < N; i++)
         cin >> num[i];
     sort(num, num + N);
-    vector<int> v;
-    for(int i = 0; i < M; i++)
-        v.push_back(0);
-    for(int i = 0; i < N - M; i++)
-        v.push_back(1);
-    do{
-        for(int i = 0; i < N; i++)
-            if(v[i] == 0)
-                cout << num[i] << ' ';
-        cout << '\n';
-    } while(next_permutation(v.begin(), v.end()));
+    permutation(0, 0);
     
     return 0;
 }
