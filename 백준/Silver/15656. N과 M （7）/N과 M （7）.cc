5d0905e8a5b82d5c252fc516db0efa7b61c4d5dd
lhyeonj120 <56513436+lhyeonj120@users.noreply.github.com>
2022-11-22 18:36:48 +0900

    [Silver III] Title: N과 M (7), Time: 472 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int num[10];
+int arr[10];
+
+void permutation(int cnt){
+    if(cnt == M){
+        for(int i = 0; i < M; i++)
+            cout << num[arr[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+    for(int i = 0; i < N; i++){
+        arr[cnt] = i;
+        permutation(cnt + 1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> N >> M;
+    for(int i = 0; i < N; i++)
+        cin >> num[i];
+    sort(num, num + N);
+    permutation(0);
+    
+    return 0;
+}
